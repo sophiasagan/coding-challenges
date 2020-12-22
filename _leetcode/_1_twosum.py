62a5bac32f80957d78898ab5d9536773b3928281
Sophia Jung <sophiabraddock82@gmail.com>
2020-12-21 23:25:25 -0500

    reverse integers

--- _leetcode/_1_twosum.py
+++ _leetcode/_1_twosum.py
@@ -1,10 +1,20 @@
-class Solution:
-    def twoSum(self, nums, target):
-        ht = {} # init dict
-        if not nums: # base case
-            return []
-        for key, num in enumerate(nums): # iterate and add counter
-            if ht.get(target - num) is not None: # get value - 
-                return [key, ht.get(target - num)]
-            ht[num] = key
+
+def twoSum(nums, target):
+    ht = {} # init dict
+    if not nums: # base case
+        return []
+    for key, num in enumerate(nums): # iterate and add counter
+        if ht.get(target - num) is not None: # get value - 
+            return [key, ht.get(target - num)]
+        ht[num] = key
         
+# nums = [2, 7, 11, 15]
+# target = 9
+print(twoSum([1, 2], 3))
+print(twosum([6, 2, 1, 7, 11, 15], 7))
+
+print(twosum([2, 7, 11, 15], 9))
+
+print(twosum([0, 4, 3, 0], 3))
+
+print(twosum([-3,4,3,9], 0))
---
+++ _leetcode/_7_reverse_integer.py
@@ -0,0 +1,65 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        res = 0
+        if x > 0:
+            res = int(str(x)[::-1])
+        if x < 0:
+            res = int(("-" + str(x * -1)[::-1]))
+        if res <= -2**31 or res >= 2**31:
+            return 0
+        else:
+            return res
+
+
+# initial solution 
+class Solution:
+    def reverse(self, x: int) -> int:
+        x = str(x)
+        return str(x)[::-1]
+
+Input:
+-123
+Output:
+321-
+Expected:
+-321
+
+# second working solution
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        if x >= 0:
+            x = str(x)
+            return str(x)[::-1]
+        if x < 0:
+            x = x * -1
+            x = str(x)
+            return int((str(x)[::-1]))*-1
+
+Input:
+120
+Output:
+021
+Expected:
+21
+
+# 3rd 
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x <= -2**31 or x >= 2**31:
+            return 0
+        
+        if x >= 0:
+            x = str(x)
+            return int(str(x)[::-1])
+        if x < 0:
+            x = x * -1
+            x = str(x)
+            return int((str(x)[::-1]))*-1
+
+Input:
+1534236469
+Output:
+9646324351
+Expected:
+0
